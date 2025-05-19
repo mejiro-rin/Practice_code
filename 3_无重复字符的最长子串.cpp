4486d682f9cd6e15b2d0786e919225224e6b4d7d
Xiaoyang Qin <qxy20050511@gmail.com>
2025-05-19 20:42:37 +0800

    5.19于宿舍

--- 3_无重复字符的最长子串.cpp
+++ 3_无重复字符的最长子串.cpp
@@ -3,29 +3,59 @@
 #include <map>
 using namespace std;
 
+
+//1.暴力破解法
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+//         int len=0;
+//         for(int i = 0; i < s.size(); i++){
+//             map<int, int, greater<int>> mymap;
+//             int j = i;
+//             for(; j < s.size(); ++j){
+//                 mymap[s[j]]++;
+//                 if(mymap[s[j]] > 1){
+                    
+//                     break;
+//                 }
+//                 // if(mymap.begin()->second > 1){
+//                 //     len = max(len, j - i);
+//                 //     break;
+//                 // }
+//             }
+//             len = max(len, j - i);
+//         }
+//         return len;
+//     }
+// };
+
+//2.记录法
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int len=0;
-        for(int i = 0; i < s.size(); i++){
-            map<int, int, greater<int>> mymap;
-            int j = i;
-            for(; j < s.size(); ++j){
-                mymap[s[j]]++;
-                if(mymap[s[j]] > 1){
-                    
-                    break;
+        int mp[26] = {-1};//记录上一次出现同一字符的坐标
+        int key = 0;//记录空格数量
+        int last = 0;//记录上一次发生重复的坐标，
+        int max_len = 0;//记录当前最大长度
+        for(int i = 0; i < s.size(); ++i){
+            if(s[i] != ' '){
+                int num = s[i] - 'a';
+                if(mp[num] == -1){
+                    mp[num] = i;
                 }
-                // if(mymap.begin()->second > 1){
-                //     len = max(len, j - i);
-                //     break;
-                // }
+                else{
+                    int len = i - last;
+                    max_len = max(len, max_len); 
+                    last = mp[num];
+                    mp[num] = i;
+                }
             }
-            len = max(len, j - i);
+            else key++; 
         }
-        return len;
+        return max_len - key;
     }
 };
+
 
 int main() {
     Solution solution;
--- 4_寻找两个正序数组的中位数.cpp
+++ 4_寻找两个正序数组的中位数.cpp
@@ -3,43 +3,83 @@
 using namespace std;
 
 
+//1.合并数组法
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int i = 0, j = 0;
+    double findMedianSortedArrays(const vector<int>& nums1, const vector<int>& nums2) {
         int m = nums1.size(), n = nums2.size();
-        while((i + j) < ((m + n - 1) / 2)){
-            if(i == m || j == n) break;
-            if(nums1[i] < nums2[j]){
-                i++;
+        int k = m + n, mid = k / 2;
+        int i = 0, j = 0;
+        vector<int> nums3;
+
+        // 合并两个数组直到达到中位数位置
+        while (nums3.size() <= mid) {
+            if (i < m && (j >= n || nums1[i] <= nums2[j])) {
+                nums3.push_back(nums1[i++]);
             }
-            else if(nums1[i] > nums2[j]){
-                j++;
-            }
-            else{
-                i++;
-                j++;
-            }
-        }
-        if(i != m){
-            while((i + j) < ((m + n - 1) / 2)){
-                i++;
+            else if (j < n) {
+                nums3.push_back(nums2[j++]);
             }
         }
-        else if(j != n){
-            while((i + j) < ((m + n - 1) / 2)){
-                j++;
-            }
+
+        // 根据总长度的奇偶性返回中位数
+        if (k % 2 == 0) {
+            return (nums3[mid - 1] + nums3[mid]) / 2.0;
         }
-        cout << i << " " << j << endl;
+        else {
+            return nums3[mid];
+        }
     }
 };
 
+
+// 2.双指针
+// class Solution {
+// public:
+//     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+//         int i = 0, j = 0;
+//         int m = nums1.size(), n = nums2.size();
+//         while((i + j + 1) < ((m + n) / 2)){
+//             if(i == m || j == n) break;
+//             if(nums1[i] < nums2[j]){
+//                 i++;
+//             }
+//             else if(nums1[i] > nums2[j]){
+//                 j++;
+//             }
+//             else{
+//                 i++;
+//                 j++;
+//             }
+//         }
+//         if(i != m){
+//             while((i + j + 1) < ((m + n) / 2)){
+//                 i++;
+//             }
+//         }
+//         else if(j != n){
+//             while((i + j + 1) < ((m + n) / 2)){
+//                 j++;
+//             }
+//         }
+//         double ans = 0;
+//         if((m + n) % 2 == 0){
+//             ans = (nums1[i] + nums2[j]) / 2.0;
+//         }
+//         else{
+//             ans = min(nums1[i], nums2[j]) * 1.0;
+//         }
+//         return ans;
+//     }
+// };
+
+
+
 int main() {
     Solution solution;
-    vector<int> nums1 = {1, 3};
-    vector<int> nums2 = {2, 5, 6, 7};
-    // cout << solution.findMedianSortedArrays(nums1, nums2) << endl;
-    solution.findMedianSortedArrays(nums1, nums2);
+    vector<int> nums1 = {1, 2};
+    vector<int> nums2 = {3, 4};
+    double result = solution.findMedianSortedArrays(nums1, nums2);
+    cout << result << endl;
     return 0;
 }
